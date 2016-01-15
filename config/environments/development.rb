b960c985083c04495bf3ea9a00189944eae6ac05
Vlad Hietala <vlad.hietata@hotmail.com>
2016-01-15 18:16:37 +0000

    Adicionar configuração de envio de e-mail

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -26,7 +26,22 @@ Rails.application.configure do
   # This option may cause significant delays in view rendering with a large
   # number of complex assets.
   config.assets.debug = true
+  
+  config.action_mailer.smtp_settings = {
+    address: "smtp.mandrillapp.com",
+    port: 587,
+    user_name: Rails.application.secrets.mandrill_username,
+    password: Rails.application.secrets.mandrill_api_key
+  }
+  
+  # ActionMailer Config
+  config.action_mailer.default_url_options = { :host => 'localhost:3000' }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.raise_delivery_errors = true
 
+  # Send email in development mode?
+  config.action_mailer.perform_deliveries = true
+  
   # Asset digests allow you to set far-future HTTP expiration dates on all assets,
   # yet still be able to expire them through the digest params.
   config.assets.digest = true
