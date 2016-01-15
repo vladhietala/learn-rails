73bc2664221661a2a45851bf26985eafe633106e
Vlad Hietala <vlad.hietata@hotmail.com>
2016-01-15 18:44:11 +0000

    Testar public/index

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,10 @@ Rails.application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
+  
+  Rails.application.routes.draw do
+    root to: redirect('/about.html')
+  end
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
